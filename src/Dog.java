b00530ac732d90acd3958d83157d4c80522030df
KAHK <kaaghika@gmail.com>
2018-12-23 23:47:29 +0100

    Just first step

---
+++ src/Dog.java
@@ -0,0 +1,7 @@
+public class Dog extends Animal {
+    private String size;
+
+    public Dog (String name, Integer age, String size){
+
+    }
+}
---
+++ src/OtherAnimal.java
@@ -0,0 +1,4 @@
+public class OtherAnimal extends Animal {
+    private String species;
+    private String other; //room to place another important info of this species
+}
